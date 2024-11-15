c302f2ff558d4865d4bf7818c397a3e6af05e7c6
Amit Trivedi <amitkumar.trivedi@lseg.com>
2024-11-15 11:42:31 +0530

    Add missing files and .gitignore

---
+++ LayeredArchitecture/Task1OnionArchitecture/OnionArch_CartService/Persistence/CartLiteDB.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Entities;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    /// <summary>
+    /// https://www.litedb.org/docs/getting-started/
+    /// </summary>
+    internal class CartLiteDB
+    {
+        private LiteDatabase GetDB()
+        {
+            // Open database (or create if doesn't exist)
+            return new LiteDatabase(@"C:\Temp\CartServiceData.db");
+        }
+
+        private ILiteCollection<Cart> GetCartsCollection(LiteDatabase db)
+        {
+            // Get a collection (or create, if doesn't exist)
+            return db.GetCollection<Cart>("crarts");
+        }
+
+        public bool InsertCart(Cart cart)
+        {
+            using (var db = GetDB())
+            {                
+                var cartsCollection = GetCartsCollection(db);
+                // Insert new document (Id will be auto-incremented)
+                cartsCollection.Insert(cart);
+            }
+
+            return true;
+        }
+
+        public bool UpdateCart(Cart cart)
+        {
+            using (var db = GetDB())
+            {
+                var cartsCollection = GetCartsCollection(db);
+                // Insert new document (Id will be auto-incremented)
+                cartsCollection.Update(cart);
+            }
+
+            return true;
+        }
+
+        public Cart GetCart(int cartId)
+        {
+            using (var db = GetDB())
+            {
+                var cartsCollection = GetCartsCollection(db);
+                return cartsCollection.Query()
+                    .Where(x => x.CartId == cartId).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ LayeredArchitecture/Task1OnionArchitecture/OnionArch_CartService/Services/ExtentionMethods.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Entities;
+using Shared.DTO;
+
+namespace Services
+{
+    internal static class ExtentionMethods
+    {
+        public static CartItem ToCartItem(this CartItemDTO cartDTO)
+        {
+            CartItem cartItem = new CartItem()
+            {
+                ItemId = cartDTO.ItemId,
+                Name = cartDTO.Name,
+                Price = cartDTO.Price,
+                Quantity = cartDTO.Quantity
+            };
+
+            if (cartDTO.ItemImage != null)
+            {
+                cartItem.ItemImage = new ItemImageInfo()
+                {
+                    Url = cartDTO.ItemImage.Url,
+                    AltText = cartDTO.ItemImage.AltText,
+                };
+            }
+            return cartItem;
+        }
+
+        public static CartItemDTO ToCartItemDTO(this CartItem cartItem,int cartId)
+        {
+            CartItemDTO cartItemDTO = new CartItemDTO()
+            {
+                CartId = cartId,
+                ItemId = cartItem.ItemId,
+                Name = cartItem.Name,
+                Price = cartItem.Price,
+                Quantity = cartItem.Quantity
+            };
+
+            if (cartItem.ItemImage != null)
+            {
+                cartItemDTO.ItemImage = new ItemImageInfoDTO()
+                {
+                    Url = cartItem.ItemImage.Url,
+                    AltText = cartItem.ItemImage.AltText,
+                };
+            }
+            return cartItemDTO;
+        }
+    }
+}
